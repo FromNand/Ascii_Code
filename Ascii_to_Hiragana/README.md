98e6c34792f8fca65f14e660984a9ed0eeb5da24
FromNand <celestial.folt@gmail.com>
2020-08-24 11:35:33 +0900

    improved conversion of 2 roman letters to hiragana

--- Ascii_to_Hiragana/README.md
+++ Ascii_to_Hiragana/README.md
@@ -2,11 +2,10 @@
 
 ascii_to_romajiとは、「48 65 6c 6c 6f 20 57 6f 72 6c 64 21 21 0a」のようなアスキーコードを表現したテキストをローマ字に変換するツール。
 入力ファイルには、2桁の16進数を空白やタブ(実は0~9, A~F, a~f以外の文字ならなんでもいい)で区切って記述してください。
-
 アスキーテキストの一行の最大文字数は1000文字。
-それ以上は切り捨てられてしまうので、それ以上の文字数が必要な場合は改行してください
+それ以上は切り捨てられてしまうので、それ以上の文字数が必要な場合は改行してください。
 
-ascii.txt(入力ファイル)に記述するアスキーテキストの例
+[ascii.txt(入力ファイル)に記述するアスキーテキストの例]
 
 23 69 6e 63 6c 75 64 65 3c 73 74 64 69 6f 2e 68 3e 0a 0a
 69 6e 74 20 6d 61 69 6e 28 76 6f 69 64 29 7b 0a 09
@@ -17,7 +16,7 @@ ascii.txt(入力ファイル)に記述するアスキーテキストの例
 72 65 74 75 72 6e 20 30 3b 0a
 7d
 
-上記のascii.txt(入力ファイル)をascii_to_romajiを使用して変換した結果
+[出力結果]
 
 #include<stdio.h>
 
@@ -37,10 +36,213 @@ romaji_to_hiraganaとは「boku ha uchuujinnda!!」といったローマ字が�
 
 入力ファイルにはアスキーコードで表現される任意の文字が使用できます。
 ただし、空白やタブ、改行を除いた制御文字は無視されることに注意してください。
-あと、非常に致命的なのですが「あいうえお　かきくけこ　...　らりるれろ　わをん」の50音以外は実装できていません。
-つまり、「だ」や「しゅ」といったものを入力するとうまくプログラムが動作しません。
-
-具体的にどのような変換が行われているかというと、例えば「ha」という文字列は「は」と置き換えられるが、「!」という文字は変換が行われないといったように、アルファベットの組み合わせをひらがなに変換する以外のことはやっていません。
+あと、非常に致命的なのですが、3文字以上のローマ字から生成される日本語には対応できていません。
+例えば「りゃ(rya)」や「しゅ(shu)」といったものを入力するとうまくプログラムが動作しませんが、「じゃ(ja)」や「ふぁ(fa)」はうまく動作します。
+ちなみに、ローマ字以外の文字は変換されずに結果にファイルにも含まれます。
 
 ローマ字テキストの一行の最大文字数は1000文字。
 それ以上は切り捨てられてしまうので、それ以上の文字数が必要な場合は改行してください。
+
+[romaji.txt(入力ファイル)に記述するローマ字テキストの例]
+
+[BOINN + BOINN]
+
+AIUEO
+KAKIKUKEKO
+SASISUSESO
+TATITUTETO
+NANINUNENO
+HAHIHUHEHO
+MAMIMUMEMO
+YAYUYO
+RARIRURERO
+WAWONN
+
+[BOINN + SIINN]
+
+AIUEO
+KaKiKuKeKo
+SaSiSuSeSo
+TaTiTuTeTo
+NaNiNuNeNo
+HaHiHuHeHo
+MaMiMuMeMo
+YaYuYo
+RaRiRuReRo
+WaWoNn
+
+[SIINN + BOINN]
+
+aiueo
+kAkIkUkEkO
+sAsIsUsEsO
+tAtItUtEtO
+nAnInUnEnO
+hAhIhUhEhO
+mAmImUmEmO
+yAyUyO
+rArIrUrErO
+wAwOnN
+
+[SIINN + SIINN]
+
+aiueo
+kakikukeko
+sasisuseso
+tatituteto
+naninuneno
+hahihuheho
+mamimumemo
+yayuyo
+rarirurero
+wawonn
+
+[BOINN + BOINN]
+
+GAGIGUGEGO
+ZAZIZUZEZO
+DADIDUDEDO
+BABIBUBEBO
+PAPIPUPEPO
+LALILULELO
+VU
+FAFIFUFEFO
+JAJIJUJEJO
+
+[BOINN + SIINN]
+
+GaGiGuGeGo
+ZaZiZuZeZo
+DaDiDuDeDo
+BaBiBuBeBo
+PaPiPuPePo
+LaLiLuLeLo
+Vu
+FaFiFuFeFo
+JaJiJuJeJo
+
+[SIINN + BOINN]
+
+gAgIgUgEgO
+zAzIzUzEzO
+dAdIdUdEdO
+bAbIbUbEbO
+pApIpUpEpO
+lAlIlUlElO
+vU
+fAfIfUfEfO
+jAjIjUjEjO
+
+[SIINN + SIINN]
+
+gagigugego
+zazizuzezo
+dadidudedo
+babibubebo
+papipupepo
+lalilulelo
+vu
+fafifufefo
+jajijujejo
+
+[出力結果]
+
+[ぼいん + ぼいん]
+
+あいうえお
+かきくけこ
+さしすせそ
+たちつてと
+なにぬねの
+はひふへほ
+まみむめも
+やゆよ
+らりるれろ
+わをん
+
+[ぼいん + しいん]
+
+あいうえお
+かきくけこ
+さしすせそ
+たちつてと
+なにぬねの
+はひふへほ
+まみむめも
+やゆよ
+らりるれろ
+わをん
+
+[しいん + ぼいん]
+
+あいうえお
+かきくけこ
+さしすせそ
+たちつてと
+なにぬねの
+はひふへほ
+まみむめも
+やゆよ
+らりるれろ
+わをん
+
+[しいん + しいん]
+
+あいうえお
+かきくけこ
+さしすせそ
+たちつてと
+なにぬねの
+はひふへほ
+まみむめも
+やゆよ
+らりるれろ
+わをん
+
+[ぼいん + ぼいん]
+
+がぎぐげご
+ざじずぜぞ
+だぢづでど
+ばびぶべぼ
+ぱぴぷぺぽ
+ぁぃぅぇぉ
+ゔ
+ふぁふぃふふぇふぉ
+じぁじじゅじぇじょ
+
+[ぼいん + しいん]
+
+がぎぐげご
+ざじずぜぞ
+だぢづでど
+ばびぶべぼ
+ぱぴぷぺぽ
+ぁぃぅぇぉ
+ゔ
+ふぁふぃふふぇふぉ
+じぁじじゅじぇじょ
+
+[しいん + ぼいん]
+
+がぎぐげご
+ざじずぜぞ
+だぢづでど
+ばびぶべぼ
+ぱぴぷぺぽ
+ぁぃぅぇぉ
+ゔ
+ふぁふぃふふぇふぉ
+じぁじじゅじぇじょ
+
+[しいん + しいん]
+
+がぎぐげご
+ざじずぜぞ
+だぢづでど
+ばびぶべぼ
+ぱぴぷぺぽ
+ぁぃぅぇぉ
+ゔ
+ふぁふぃふふぇふぉ
+じぁじじゅじぇじょ
--- Ascii_to_Hiragana/romaji_to_hiragana.c
+++ Ascii_to_Hiragana/romaji_to_hiragana.c
@@ -16,90 +16,165 @@ int is_alphabet(char c){
 int one_character(char *s1, char *s2){
 	s2[0] = 0xe3;
 	s2[1] = 0x81;
-	if(*s1 == 'A'){s2[2] = 0x82; return 0;}
-	else if(*s1 == 'I'){s2[2] = 0x84; return 0;}
-	else if(*s1 == 'U'){s2[2] = 0x86; return 0;}
-	else if(*s1 == 'E'){s2[2] = 0x88; return 0;}
-	else if(*s1 == 'O'){s2[2] = 0x8a; return 0;}
-	return 1;
+	if(*s1 == 'A'){s2[2] = 0x82; return 3;}
+	else if(*s1 == 'I'){s2[2] = 0x84; return 3;}
+	else if(*s1 == 'U'){s2[2] = 0x86; return 3;}
+	else if(*s1 == 'E'){s2[2] = 0x88; return 3;}
+	else if(*s1 == 'O'){s2[2] = 0x8a; return 3;}
+	return 0;
 }
 
 int two_character(char *s1, char *s2){
 	s2[0] = 0xe3;
 	if(s1[0] == 'K'){
 		s2[1] = 0x81;
-		if(s1[1] == 'A'){s2[2] = 0x8b; return 0;}
-		if(s1[1] == 'I'){s2[2] = 0x8d; return 0;}
-		if(s1[1] == 'U'){s2[2] = 0x8f; return 0;}
-		if(s1[1] == 'E'){s2[2] = 0x91; return 0;}
-		if(s1[1] == 'O'){s2[2] = 0x93; return 0;}
+		if(s1[1] == 'A'){s2[2] = 0x8b; return 3;}
+		if(s1[1] == 'I'){s2[2] = 0x8d; return 3;}
+		if(s1[1] == 'U'){s2[2] = 0x8f; return 3;}
+		if(s1[1] == 'E'){s2[2] = 0x91; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0x93; return 3;}
+	}
+	else if(s1[0] == 'Z'){
+		s2[1] = 0x81;
+		if(s1[1] == 'A'){s2[2] = 0x96; return 3;}
+		if(s1[1] == 'I'){s2[2] = 0x98; return 3;}
+		if(s1[1] == 'U'){s2[2] = 0x9a; return 3;}
+		if(s1[1] == 'E'){s2[2] = 0x9c; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0x9e; return 3;}
+	}
+	else if(s1[0] == 'G'){
+		s2[1] = 0x81;
+		if(s1[1] == 'A'){s2[2] = 0x8c; return 3;}
+		if(s1[1] == 'I'){s2[2] = 0x8e; return 3;}
+		if(s1[1] == 'U'){s2[2] = 0x90; return 3;}
+		if(s1[1] == 'E'){s2[2] = 0x92; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0x94; return 3;}
 	}
 	else if(s1[0] == 'S'){
 		s2[1] = 0x81;
-		if(s1[1] == 'A'){s2[2] = 0x95; return 0;}
-		if(s1[1] == 'I'){s2[2] = 0x97; return 0;}
-		if(s1[1] == 'U'){s2[2] = 0x99; return 0;}
-		if(s1[1] == 'E'){s2[2] = 0x9b; return 0;}
-		if(s1[1] == 'O'){s2[2] = 0x9d; return 0;}
+		if(s1[1] == 'A'){s2[2] = 0x95; return 3;}
+		if(s1[1] == 'I'){s2[2] = 0x97; return 3;}
+		if(s1[1] == 'U'){s2[2] = 0x99; return 3;}
+		if(s1[1] == 'E'){s2[2] = 0x9b; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0x9d; return 3;}
 	}
 	else if(s1[0] == 'T'){
 		s2[1] = 0x81;
-		if(s1[1] == 'A'){s2[2] = 0x9f; return 0;}
-		if(s1[1] == 'I'){s2[2] = 0xa1; return 0;}
-		if(s1[1] == 'U'){s2[2] = 0xa4; return 0;}
-		if(s1[1] == 'E'){s2[2] = 0xa6; return 0;}
-		if(s1[1] == 'O'){s2[2] = 0xa8; return 0;}
+		if(s1[1] == 'A'){s2[2] = 0x9f; return 3;}
+		if(s1[1] == 'I'){s2[2] = 0xa1; return 3;}
+		if(s1[1] == 'U'){s2[2] = 0xa4; return 3;}
+		if(s1[1] == 'E'){s2[2] = 0xa6; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0xa8; return 3;}
+	}
+	else if(s1[0] == 'D'){
+		s2[1] = 0x81;
+		if(s1[1] == 'A'){s2[2] = 0xa0; return 3;}
+		if(s1[1] == 'I'){s2[2] = 0xa2; return 3;}
+		if(s1[1] == 'U'){s2[2] = 0xa5; return 3;}
+		if(s1[1] == 'E'){s2[2] = 0xa7; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0xa9; return 3;}
 	}
 	else if(s1[0] == 'N'){
 		s2[1] = 0x81;
-		if(s1[1] == 'A'){s2[2] = 0xaa; return 0;}
-		if(s1[1] == 'I'){s2[2] = 0xab; return 0;}
-		if(s1[1] == 'U'){s2[2] = 0xac; return 0;}
-		if(s1[1] == 'E'){s2[2] = 0xad; return 0;}
-		if(s1[1] == 'O'){s2[2] = 0xae; return 0;}
-		if(s1[1] == 'N'){s2[1] = 0x82; s2[2] = 0x93; return 0;}
+		if(s1[1] == 'A'){s2[2] = 0xaa; return 3;}
+		if(s1[1] == 'I'){s2[2] = 0xab; return 3;}
+		if(s1[1] == 'U'){s2[2] = 0xac; return 3;}
+		if(s1[1] == 'E'){s2[2] = 0xad; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0xae; return 3;}
+		if(s1[1] == 'N'){s2[1] = 0x82; s2[2] = 0x93; return 3;}
 	}
 	else if(s1[0] == 'H'){
 		s2[1] = 0x81;
-		if(s1[1] == 'A'){s2[2] = 0xaf; return 0;}
-		if(s1[1] == 'I'){s2[2] = 0xb2; return 0;}
-		if(s1[1] == 'U'){s2[2] = 0xb5; return 0;}
-		if(s1[1] == 'E'){s2[2] = 0xb8; return 0;}
-		if(s1[1] == 'O'){s2[2] = 0xbb; return 0;}
+		if(s1[1] == 'A'){s2[2] = 0xaf; return 3;}
+		if(s1[1] == 'I'){s2[2] = 0xb2; return 3;}
+		if(s1[1] == 'U'){s2[2] = 0xb5; return 3;}
+		if(s1[1] == 'E'){s2[2] = 0xb8; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0xbb; return 3;}
+	}
+	else if(s1[0] == 'B'){
+		s2[1] = 0x81;
+		if(s1[1] == 'A'){s2[2] = 0xb0; return 3;}
+		if(s1[1] == 'I'){s2[2] = 0xb3; return 3;}
+		if(s1[1] == 'U'){s2[2] = 0xb6; return 3;}
+		if(s1[1] == 'E'){s2[2] = 0xb9; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0xbc; return 3;}
+	}
+	else if(s1[0] == 'P'){
+		s2[1] = 0x81;
+		if(s1[1] == 'A'){s2[2] = 0xb1; return 3;}
+		if(s1[1] == 'I'){s2[2] = 0xb4; return 3;}
+		if(s1[1] == 'U'){s2[2] = 0xb7; return 3;}
+		if(s1[1] == 'E'){s2[2] = 0xba; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0xbd; return 3;}
 	}
 	else if(s1[0] == 'M'){
-		if(s1[1] == 'A'){s2[1] = 0x81; s2[2] = 0xbe; return 0;}
-		if(s1[1] == 'I'){s2[1] = 0x81; s2[2] = 0xbf; return 0;}
-		if(s1[1] == 'U'){s2[1] = 0x82; s2[2] = 0x80; return 0;}
-		if(s1[1] == 'E'){s2[1] = 0x82; s2[2] = 0x81; return 0;}
-		if(s1[1] == 'O'){s2[1] = 0x82; s2[2] = 0x82; return 0;}
+		if(s1[1] == 'A'){s2[1] = 0x81; s2[2] = 0xbe; return 3;}
+		if(s1[1] == 'I'){s2[1] = 0x81; s2[2] = 0xbf; return 3;}
+		if(s1[1] == 'U'){s2[1] = 0x82; s2[2] = 0x80; return 3;}
+		if(s1[1] == 'E'){s2[1] = 0x82; s2[2] = 0x81; return 3;}
+		if(s1[1] == 'O'){s2[1] = 0x82; s2[2] = 0x82; return 3;}
 	}
 	else if(s1[0] == 'Y'){
 		s2[1] = 0x82;
-		if(s1[1] == 'A'){s2[2] = 0x84; return 0;}
-		if(s1[1] == 'U'){s2[2] = 0x86; return 0;}
-		if(s1[1] == 'O'){s2[2] = 0x88; return 0;}
+		if(s1[1] == 'A'){s2[2] = 0x84; return 3;}
+		if(s1[1] == 'U'){s2[2] = 0x86; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0x88; return 3;}
 	}
 	else if(s1[0] == 'R'){
 		s2[1] = 0x82;
-		if(s1[1] == 'A'){s2[2] = 0x89; return 0;}
-		if(s1[1] == 'I'){s2[2] = 0x8a; return 0;}
-		if(s1[1] == 'U'){s2[2] = 0x8b; return 0;}
-		if(s1[1] == 'E'){s2[2] = 0x8c; return 0;}
-		if(s1[1] == 'O'){s2[2] = 0x8d; return 0;}
+		if(s1[1] == 'A'){s2[2] = 0x89; return 3;}
+		if(s1[1] == 'I'){s2[2] = 0x8a; return 3;}
+		if(s1[1] == 'U'){s2[2] = 0x8b; return 3;}
+		if(s1[1] == 'E'){s2[2] = 0x8c; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0x8d; return 3;}
 	}
 	else if(s1[0] == 'W'){
 		s2[1] = 0x82;
-		if(s1[1] == 'A'){s2[2] = 0x8f; return 0;}
-		if(s1[1] == 'O'){s2[2] = 0x92; return 0;}
+		if(s1[1] == 'A'){s2[2] = 0x8f; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0x92; return 3;}
 	}
-	return 1;
+	else if(s1[0] == 'V' && s1[1] == 'U'){
+		s2[1] = 0x82;
+		s2[2] = 0x94;
+		return 3;
+	}
+	else if(s1[0] == 'L'){
+		s2[1] = 0x81;
+		if(s1[1] == 'A'){s2[2] = 0x81; return 3;}
+		if(s1[1] == 'I'){s2[2] = 0x83; return 3;}
+		if(s1[1] == 'U'){s2[2] = 0x85; return 3;}
+		if(s1[1] == 'E'){s2[2] = 0x87; return 3;}
+		if(s1[1] == 'O'){s2[2] = 0x89; return 3;}
+	}
+	else if(s1[0] == 'F'){
+		s2[1] = 0x81;
+		s2[2] = 0xb5;
+		s2[3] = 0xe3;
+		s2[4] = 0x81;
+		if(s1[1] == 'A'){s2[5] = 0x81; return 6;}
+		if(s1[1] == 'I'){s2[5] = 0x83; return 6;}
+		if(s1[1] == 'U'){s2[2] = 0xb5; return 3;}
+		if(s1[1] == 'E'){s2[5] = 0x87; return 6;}
+		if(s1[1] == 'O'){s2[5] = 0x89; return 6;}
+	}
+	else if(s1[0] == 'J'){
+		s2[1] = 0x81;
+		s2[2] = 0x98;
+		s2[3] = 0xe3;
+		s2[4] = 0x82;
+		if(s1[1] == 'A'){s2[4] = 0x81; s2[5] = 0x81; return 6;}
+		if(s1[1] == 'I'){s2[2] = 0x98; return 3;}
+		if(s1[1] == 'U'){s2[5] = 0x85; return 6;}
+		if(s1[1] == 'E'){s2[4] = 0x81; s2[5] = 0x87; return 6;}
+		if(s1[1] == 'O'){s2[5] = 0x87; return 6;}
+	}
+	return 0;
 }
 
 int main(int argc, char **argv){
 	char *buffer, *string, *string_top;
 	FILE *fp;
-	int i, length;
+	int i, length, character_size;
 
 	if(argc != 2){
 		printf("引数に変換したいローマ字テキストを指定してください。\n");
@@ -111,7 +186,7 @@ int main(int argc, char **argv){
 		exit(2);
 	}
 
-	if(!(string = malloc(3000))){
+	if(!(string = malloc(6000))){
 		printf("出力用バッファのメモリが確保できませんでした。\n");
 		exit(3);
 	}
@@ -136,7 +211,7 @@ int main(int argc, char **argv){
 			if(is_alphabet(buffer[i]) == 2) buffer[i] -= 0x20;
 			//もし一文字目が母音ならば即ひらがな(カタカナ)に変換する
 			if(is_lower_vowel(buffer[i])){
-				if(!one_character(buffer + i, string)) string += 3;
+				if(character_size = one_character(buffer + i, string)) string += character_size;
 				else{printf("ローマ字の一文字変換に失敗しました。\n"); exit(5);}
 				i += 1;
 				continue;
@@ -147,13 +222,13 @@ int main(int argc, char **argv){
 			//もし二文字目が母音ならば一文字目との組み合わせでひらがな(カタカナ)に変換する
 			//ん(NN)は例外なので、or条件を使用して処理する
 			if(is_lower_vowel(buffer[i+1]) || !strncmp(buffer + i, "NN", 2)){
-				if(!two_character(buffer + i, string)) string += 3;
+				if(character_size = two_character(buffer + i, string)) string += character_size;
 				else{printf("ローマ字の二文字変換に失敗しました。\n"); exit(6);}
 				i += 2;
 				continue;
 			}
 
-			//ここまでくるということは「だ」や「しゃ」といった実装されていない文字を書いたか、単なる入力ミスということになる
+			//ここまでくるということは「りゃ」や「しゃ」といった実装されていない文字を書いたか、子音を単体で書いているなどの入力ミスということになる
 			printf("実装されていない文字を入力したか、入力した文字がおかしいです。\n");
 			exit(7);
 		}
